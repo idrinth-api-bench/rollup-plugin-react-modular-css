e84ce35a017aad693dd99b1418918642069997bf
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-28 21:44:32 +0100

    version 1.0.3

--- src/load.ts
+++ src/load.ts
@@ -1,11 +1,11 @@
-
-const header = document.getElementsByTagName('header')[0];
+const global = window || this || {};
+const header = global.document.getElementsByTagName('header')[0];
 export default (hash: string, name: string) => {
     const path = `/assets/${name}-${hash}.min.css`;
-    if (document.querySelector(`link[href='${ path }']`)) {
+    if (global.document.querySelector(`link[href='${ path }']`)) {
         return;
     }
-    const style= document.createElement('link');
+    const style= global.document.createElement('link');
     style.setAttribute('rel','stylesheet');
     style.setAttribute('href', path);
     header.appendChild(style);
