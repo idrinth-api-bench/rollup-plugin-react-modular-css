2ef0f181c6f4321101f77ba546106f89e5ffcd36
Björn Büttner <Idrinth@users.noreply.github.com>
2024-06-14 15:03:12 +0200

    actually use the vite base url when available

--- src/load.ts
+++ src/load.ts
@@ -1,7 +1,9 @@
 export default (hash: string, name: string) => {
     const global = window || this || {};
     const header = global.document.getElementsByTagName('head')[0];
-    const path = `/assets/${name}-${hash}.min.css`;
+    // @ts-ignore
+    const prefix= import.meta.env.BASE_URL ?? '';
+    const path = `${prefix}/assets/${name}-${hash}.min.css`;
     if (global.document.querySelector(`link[href='${ path }']`)) {
         return;
     }
